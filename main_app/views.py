20a1b59ce73693a23e095311b9191a57c5fea963
Conor Furlong <cfurlong15@gmail.com>
2024-01-11 23:55:53 -0500

    complete part 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Game
 
 # games = [
@@ -25,3 +25,15 @@ def games_index(request):
 def games_detail(request, game_id):
     game = Game.objects.get(id=game_id)
     return render(request, 'games/detail.html', {'game': game})
+
+class GameCreate(CreateView):
+    model = Game
+    fields = '__all__'
+
+class GameUpdate(UpdateView):
+    model = Game
+    fields = '__all__'
+
+class GameDelete(DeleteView):
+    model = Game
+    success_url = '/games'
